7d8661200d46c1c70ae09cd869fd4a2c6db68bbb
Sebastien Jachym <sebastien.jachym@agri-commerce.fr>
2023-01-26 16:33:00 +0100

    jaccoco

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,4 +6,21 @@ public class Calculator {
         return a + b;
     }
 
+    public double divide(int a, int b) {
+
+        if (b == 0) {
+            System.out.println("Division par zéro");
+//            return a < 0 ?  Double.MIN_VALUE : Double.MAX_VALUE;
+            if (a == 0) {
+                return 0;
+            }
+            if (a < 0) {
+                return Double.MIN_VALUE;
+            }
+            return Double.MAX_VALUE;
+        }
+
+        return (double) a / b;
+    }
+
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,24 +1,44 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+//    Setup     : initialisation
+//    Test      : test
+//    TearDown  : fermeture des resources
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        System.out.println("beforeEach");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("afterEach");
+    }
+
     @Test
     void testSimple() {
-        Calculator calculator = new Calculator();
 
         // Assert : Vérifier
 
         Integer a = 1;
         Integer b = 2;
-        assertEquals(3, calculator.add(1,2)); // 1+ 2 = 3 ?
+        assertEquals(3, calculator.add(1, 2)); // 1+ 2 = 3 ?
 
         a = Integer.MAX_VALUE;
         b = Integer.MAX_VALUE;
@@ -26,29 +46,27 @@ class CalculatorTest {
 
         a = null;
         b = null;
-        assertEquals(3, calculator.add(a,b));
+        assertEquals(3, calculator.add(a, b));
     }
 
     @Test
     void testThrow() {
         Integer a = null;
         assertThrows(NullPointerException.class, () -> {
-            Calculator calculator = new Calculator();
             calculator.add(2, 3);
         });
     }
 
     @Test
-    @DisplayName("1 + 2 = 3 (dans le @DisplayName)") // impacte le nom du test (et pas le résultat de la ligne de test)
+    @DisplayName("1 + 2 = 3 (dans le @DisplayName)")
+        // impacte le nom du test (et pas le résultat de la ligne de test)
     void testNommé() {
-        Calculator calculator = new Calculator();
-        assertEquals(3, calculator.add(1,222)); // 1+ 2 = 3 ?
+        assertEquals(3, calculator.add(1, 222)); // 1+ 2 = 3 ?
     }
 
     @Test
     void testCommenté() {
-        Calculator calculator = new Calculator();
-        assertEquals(3, calculator.add(1,222), "1 + 2 = 3 (dans le commentaire)"); // org.opentest4j.AssertionFailedError: 1 + 2 = 3 (dans le commentaire) ==>
+        assertEquals(3, calculator.add(1, 222), "1 + 2 = 3 (dans le commentaire) [le message affiché en cas de fail]"); // org.opentest4j.AssertionFailedError: 1 + 2 = 3 (dans le commentaire) ==>
     }
 
 
@@ -59,11 +77,17 @@ class CalculatorTest {
             "-1, 1, 0",
             "50, 60, 100"
     })
-    void testAddCsv(int a, int b, int expectedResult){
-        Calculator calculator = new Calculator();
+    void testAddCsv(int a, int b, int expectedResult) {
 //        assertEquals(expectedResult, a, b); // est passant, mais pourquoi ? (oublie de l'appel a la méthode et 3eme argument)
         assertEquals(expectedResult, calculator.add(a, b));
-        assertEquals(expectedResult, 3, "message en cas de fail"); // org.opentest4j.AssertionFailedError: message en cas de fail ==>
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(0.5, calculator.divide(1, 2));
+//        assertEquals(0.333, calculator.divide(1, 3)); // 0.333 différent de 0.3333333333333333
+        assertEquals(0.333, calculator.divide(1, 3), 0.001); // plage
+        assertEquals(0 , calculator.divide(123,0));
     }
 
 }
